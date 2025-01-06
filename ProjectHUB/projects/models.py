f53f81061148913978539ce0ee94ea24e098f153
King Blue <matthiasnijman@hotmail.com>
2025-01-06 11:58:05 +0100

    Database setup

--- ProjectHUB/projects/models.py
+++ ProjectHUB/projects/models.py
@@ -1,9 +1,33 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Student(models.Model):
+    student_nr = models.IntegerField()
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30)
+    plant = models.IntegerField(blank=True, null=True)
+    investigator = models.IntegerField(blank=True, null=True)
+    coordinator = models.IntegerField(blank=True, null=True)
+    shaper = models.IntegerField(blank=True, null=True)
+    monitor = models.IntegerField(blank=True, null=True)
+    teamworker = models.IntegerField(blank=True, null=True)
+    implementer = models.IntegerField(blank=True, null=True)
+    finisher = models.IntegerField(blank=True, null=True)
+    specialist = models.IntegerField(blank=True, null=True)
 
-from django.db import models
+
+class Teacher(models.Model):
+    teacher_code = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30)
 
 
 class Project(models.Model):
-    content = models.TextField()
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500, default="A fun interdisciplinary project!")
+    teacher = models.ForeignKey(Teacher, blank=True, null=True, on_delete=models.SET_NULL)
+
+
+class ProjectStudents(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
