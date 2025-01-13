f4afcbc7d52f9f77e0b20e1988dd1c6beb2f5282
Bas <b.mellens@st.hanze.nl>
2025-01-13 12:13:43 +0100

    hoofletters

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,7 +2,7 @@ import { createWebHistory, createRouter } from 'vue-router'
 
 import HomeView from './components/views/HomeView.vue'
 import TestView from './components/views/TestView.vue'
-import TestResultView from './components/Views/TestResultView.vue'
+import TestResultView from './components/views/TestResultView.vue'
 
 const routes = [
   { path: '/', component: HomeView },
